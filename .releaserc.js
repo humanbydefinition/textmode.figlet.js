50b87187a46132718abc26a823a4ddaef18561a5
humanbydefinition <59119064+humanbydefinition@users.noreply.github.com>
2026-07-26 23:52:45 +0200

    refactor/native manifest tooling (#33)
    * docs: align textmode tooling
    
    * chore: remove outdated AGENTS.md file
    
    * chore: remove outdated issue and pull request templates
    
    * chore: remove unused example for TextmodeFigFont.fromString

--- .releaserc.js
+++
@@ -1,5 +0,0 @@
-import { createReleaseConfig } from '@textmode/release-config';
-
-export default createReleaseConfig({
-	githubAssets: ['dist/textmode.figlet.esm.js', 'dist/textmode.figlet.umd.js'],
-});
